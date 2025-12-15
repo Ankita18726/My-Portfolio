5a0f009f96fb6e756db5618330c9a461bd157148
Ankita18726 <agadre412@gmail.com>
2025-12-15 21:54:02 +0530

    committtt photos

--- my-app/src/data/projects.js
+++ my-app/src/data/projects.js
@@ -12,7 +12,7 @@ export const projects = [
     description:
       "Elder Care Harmony is a platform connecting families with professional nursing services for elder people. It offers features like location-based nurse search, nearby hospitals with maps, and seminar listings by NGOs.",
     tech: ["HTML", "JavaScript", "Pph", "CSS"],
-    image: elderCare,
+    image: "/images/ElderCare.png",
     github: "https://github.com/Ankita18726/ElderCare-Harmony"
   },
   {
@@ -21,7 +21,7 @@ export const projects = [
     description:
       "No Way Out is a horror maze game. It challenges the player to smartly navigate through maze avoiding all the obstacles and jumpscare. There are several levels where maze complexity gradually increases. It is available on playstore as well. ",
     tech: ["Unity", "JavaScript", "Blender"],
-    image: noWayOut,
+    image: "/images/Nowayout.jpg",
     github: "https://github.com/Ankita18726/No-Way-Out"
   },
   {
@@ -30,7 +30,7 @@ export const projects = [
     description:
       "Yumigo is a smart grocery delivery App where the user can select a particular dish and all the required ingredients will get added to cart automatically. User can increase or decrease the number of dishes as per requirement. It helps in reducing food wastage and makes grocery segregation simpler.",
     tech: ["React Native", "Firebase", "Next.js"],
-    image: yumigo,
+    image: "/images/Yumigo.png",
     github: "https://github.com/Ankita18726/Yumigo"
   },
   {
@@ -39,7 +39,7 @@ export const projects = [
     description:
       "BookWriter is a website which provides a platform for beginner writers on practicing writing books and poems. It provides basic template of book and poem and writer can edit and save the books accordingly. ",
     tech: ["Firebase", "Java", "Java Swing"],
-    image: bookWriter,
+    image: "/images/Bookwritter.png",
     github: "https://github.com/Ankita18726/BookWritter-A-platform-for-writters"
   },
   {
@@ -48,7 +48,7 @@ export const projects = [
     description:
       "The Face Recognition Attendance System automatically marks attendance by detecting and recognizing faces in real time using a camera. It eliminates manual entry and increases accuracy by matching faces with a stored database. The system records timestamps and stores attendance securely for quick access.",
     tech: ["Firebase", "Python", "OpenCV","NumPy"],
-    image: attendance,
+    image: "/images/Attendence.png",
     github: "https://github.com/AtharvaJahagirdar13/Attendance-using-Facial-Recognition"
   }
 ];
